1639f6105baaacf9f426eeeb7a5f210109fbe6b3
Valerie Riley <valeriesriley@gmail.com>
2020-01-09 17:33:56 -0700

    creates html file using css based on selected favorite color

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const axios = require('axios');
+const createHTML = require ('create-html');
+let color;
 
 inquirer
-.prompt([
+    .prompt([
     {
         type: "input",
         message: "What is your GitHub username?",
@@ -10,11 +13,11 @@ inquirer
     },
     {
         type: "input",
-        message: "What is your favorite color?",
+        message: "What is your favorite color (red, green, blue, or purple)?",
         name: "color"
     }
-]).then(function(response) {
-    const axios = require('axios');
+    ]).then(function(response) {
+    color = response.color.toLowerCase();
       axios.get('https://api.github.com/users/' + response.name + '?API_KEY=1f86a572b3e7bb32c847079099968c367910692a')
       .then(response => {
         //get data from api
@@ -38,17 +41,67 @@ inquirer
         console.log("Followers: " + followers);
         let following = response.data.following;
         console.log("Following: " + following);
+        const cssLink = color + ".css";
+        console.log("css link: " + cssLink);
+    
+        var html = createHTML({        
+            title: 'GitHub Info',
+            css: cssLink,
+            lang: 'en',
+            head: '<meta charset="UTF-8"> <title>GitHub Profile</title> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> <link rel="stylesheet" href="red.css">',
+            body: `<div class = "container">
+            <div class = "row">
+                <div class="card mb-3 mx-auto">
+                    <div class="row no-gutters">
+                        <div class="col-md-4">
+                            <img src="${pictureURL}" class="card-img-top bioImg" alt="avatar">
+                        </div>
+                        <div class="col-md-8">
+                            <div class="card-body mainColor">
+                                <h5 class="card-title">My name is ${fullName}</h5>
+                                <p class="card-text">${bio}</p>
+                                <p class = "card-text">${location}</p>
+                                <a href="${profileURL}" class="btn btn-primary btnColor">GitHub</a>
+                                <a href="${blogURL}" class="btn btn-primary btnColor">Blog</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class ="row">
+                <div class = "col-md-4">
+                    <div class="card">
+                        <div class="card-body bodyColor">
+                            <h5 class="card-title">Public Repositories</h5>
+                            <p class="card-text">${repo}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class = "col-md-4">
+                    <div class="card">
+                        <div class="card-body bodyColor">
+                            <h5 class="card-title">Followers</h5>
+                            <p class="card-text">${followers}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class = "col-md-4">
+                    <div class="card" >
+                        <div class="card-body bodyColor">
+                            <h5 class="card-title">Following</h5>
+                            <p class="card-text">${following}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
+        })
+        fs.writeFile('index.html', html, function (err) {
+            if (err) console.log(err)
+        })
     })
     .catch(error => {
         console.log(error);
     });
-    // fs.appendFile(response.name+".txt", JSON.stringify(response), function(err) {
 
-    //     if (err) {
-    //       return console.log(err);
-    //     }
-    //     console.log(response);
-    //     console.log("Success!");
-      
-    //   });
   });
